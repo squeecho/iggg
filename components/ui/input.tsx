b86cb2c1d2e8040a38a349059fdf4cfdaae37e8c
laptop-sb-Mac <shinnize@gmail.com>
2025-04-19 03:19:09 +0900

    fix: export Input component properly

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,3 +1,12 @@
-export function Label({ children }: { children: React.ReactNode }) {
-  return <label>{children}</label>
-}
+import * as React from 'react';
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className = '', ...props }, ref) => (
+  <input
+    ref={ref}
+    className={`border rounded px-3 py-2 w-full ${className}`}
+    {...props}
+  />
+));
+Input.displayName = 'Input';
